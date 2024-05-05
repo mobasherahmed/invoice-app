3815ad34ae0667f7f7c1f07bdfb590bcc291ebc1
Ahmed Mobasher <amobasher@netsync.com>
2024-05-05 21:14:54 +0300

    [feature] Component : add sidebar component

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { CreditsComponent } from '../credits/credits.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: true,
+  imports:[CreditsComponent,CommonModule]
+})
+export class SidebarComponent {
+  private ICON_SUN = 'icon-sun';
+  private ICON_MOON = 'icon-moon';
+
+  showCredits = false;
+
+  constructor(private themeService: ThemeService) {
+    this.themeService.setThemeOnStart();
+  }
+
+  handleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+
+  handleShowCredits(): void {
+    this.showCredits = true;
+  }
+
+  handleCloseCredits(closeCredits: boolean): void {
+    this.showCredits = closeCredits;
+  }
+
+  get isDarkTheme(): boolean {
+    return this.themeService.darkThemeSelected;
+  }
+
+  get themeIcon(): string {
+    return `./assets/img/${
+      this.isDarkTheme ? this.ICON_SUN : this.ICON_MOON
+    }.svg`;
+  }
+}
