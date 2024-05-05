b293e942f5037664cbe89b06a30cb214bb546ea3
Ahmed Mobasher <amobasher@netsync.com>
2024-05-05 21:10:46 +0300

    [feature] Component : add invoice list component

---
+++ src/app/invoices/components/invoice-list/invoice-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Invoice } from '../../interfaces/invoice.interface';
+import { UIService } from '../../../shared/services/ui.service';
+import { InvoiceItemComponent } from '../invoice-item/invoice-item.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-invoice-list',
+  templateUrl: './invoice-list.component.html',
+  standalone: true,
+  imports: [InvoiceItemComponent,CommonModule]
+})
+export class InvoiceListComponent implements OnInit {
+  @Input() invoices!: Invoice[];
+  activeFilter: string = '';
+
+  constructor(private uiService: UIService) {}
+
+  ngOnInit(): void {
+    this.uiService.activeFilter.subscribe(
+      (filter) => (this.activeFilter = filter)
+    );
+  }
+}
