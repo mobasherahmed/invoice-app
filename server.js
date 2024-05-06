174e46e11ea13c2edbb4cb7c8cd1fdca1c4f5c9f
Ahmed Mobasher <amobasher@netsync.com>
2024-05-07 01:44:48 +0300

    [feature] Backend : create login api and static users with different roles

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json(),cors());
 
 // Define the path to your JSON file
 const dataFilePath = './src/assets/data/invoices.json';
+const usersFilePath = './src/assets/data/users.json';
 
 // Initialize the data file if it doesn't exist
 if (!fs.existsSync(dataFilePath)) {
@@ -22,10 +24,33 @@ const readDataFile = () => {
  return JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
 };
 
+// Read the data file
+const readUserDataFile = () => {
+ return JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
+};
+
 // Write the data file
 const writeDataFile = (data) => {
  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
 };
+
+// POST login api
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    const users = readUserDataFile();
+    const user = users.find(u => u.username === username);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    if (user.password!== password) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const token = jwt.sign({ id: user.id }, 'your-hardcoded-secret-key', { expiresIn: 86400 }); // Expires in 24 hours.
+
+    // Return user details if login successful
+    res.status(200).json({ user: { username: user.username, name: user.name, id: user.id , email: user.email, role: user.role , token: token } });
+});
 
 // GET all items
 app.get('/api/invoices', (req, res) => {
